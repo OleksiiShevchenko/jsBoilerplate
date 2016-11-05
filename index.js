15694a854f2c905117d5c3855ba575921821615f
oleksiishevchenko <oleksiishevchenko@yahoo.com>
2016-11-05 21:05:48 +0200

    multiple changes

--- index.js
+++
@@ -1,2 +0,0 @@
-const Color = require('color');
-console.log('Hello world');
---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+
+
+import Dog from '../shared/dog';
+
+const str = 'ES6';
+
+
+const toby = new Dog('Toby');
+
+console.log(`Hello ${str}`);
+console.log('this is running');
+console.log(toby.bark());
